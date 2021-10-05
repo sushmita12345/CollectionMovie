d4872795571ba31042066ceb4caa1afa5aa255ae
Sushmita Gupta <sush18nov95@gmail.com>
2021-10-05 11:42:38 +0530

    rating added

--- src/App.js
+++ src/App.js
@@ -3,44 +3,44 @@ import "./styles.css";
 
 const movies = {
   Horror: [
-    { name: "The Conjuring", genres: "Horror", director: "James Wan", rating: 4.5 },
-    { name: "Annabelle", genres: "Horror", director: "David F. Sandberg", rating: 4.5 },
+    { name: "The Conjuring", genres: "Horror", director: "James Wan", rating: 4.5/5 },
+    { name: "Annabelle", genres: "Horror", director: "David F. Sandberg", rating: 4.6/5 },
     {
       name: "Don't knock twice",
       genres: "Horror",
-      director: "Caradog W. James", rating: 4.5
+      director: "Caradog W. James", rating: 4.2/5
     }
   ],
   Thriller: [
     {
       name: "Breathe: Into the Shadows",
       genres: "Suspense",
-      director: "Mayank Sharma", rating: 4.5
+      director: "Mayank Sharma", rating: 4.7/5
     },
     {
       name: "The Widow",
       genres: "Suspense",
       director: "Sam Donavan, Olly Blackburn", rating: 4.5
     },
-    { name: "Kruthi", genres: "Suspense", director: "Manu Warrier", rating: 4.5 }
+    { name: "Kruthi", genres: "Suspense", director: "Manu Warrier", rating: 4.1/5 }
   ],
   Adventures: [
     {
       name: "The Lord of Rings",
       genres: "mystery",
-      director: "Peter Jackson", rating: 4.5
+      director: "Peter Jackson", rating: 4.0/5
     },
     {
       name: "Wheel Of Time",
       genres: "mystery",
-      director: "Rosamund Pike, Daniel Henney, Josha Stradowski", rating: 4.5
+      director: "Rosamund Pike, Daniel Henney, Josha Stradowski", rating: 4.3/5
     },
     { name: "Harry Potter", genres: "mystery", director: "Chris Colombus", rating: 4.5 }
   ],
   Action: [
-    { name: "The Wall", genres: "Action", director: "Doug Liman", rating: 4.5 },
-    { name: "Sherlock Holmes", genres: "Action", director: "Guy Ritchie", rating: 4.5 },
-    { name: "Commando", genres: "Action", director: "Deven Bhojani", rating: 4.5 }
+    { name: "The Wall", genres: "Action", director: "Doug Liman", rating: 4.8/5 },
+    { name: "Sherlock Holmes", genres: "Action", director: "Guy Ritchie", rating: 4.7/5 },
+    { name: "Commando", genres: "Action", director: "Deven Bhojani", rating: 4.2/5 }
   ]
 };
 
